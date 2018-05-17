0dfcddee3078f4df94ab6b230b329a12c32e3190
gurvindersingh1905 <39147486+gurvindersingh1905@users.noreply.github.com>
2018-05-17 22:16:59 +0530

    Update SparkStreamingService.java

--- KafkaSparkStreaming/SparkStreamingService.java
+++ KafkaSparkStreaming/SparkStreamingService.java
@@ -15,6 +15,7 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.TaskContext;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
@@ -31,8 +32,10 @@ import scala.Tuple2;
 public class SparkStreamingService {
     public static ObjectMapper objectMapper;
     public static IOTModel iotData;
+    public static String jsonMessages;
     public static String jsonMessage;
     public static Date timestamp;
+    public static Put p;
 
     public static void main(String[] args) throws IOException {
         Map<String, String> kafkaParams = new HashMap<String, String>();
@@ -41,7 +44,7 @@ public class SparkStreamingService {
         kafkaParams.put("zookeeper.connect", "localhost:2181");
 
         SparkConf sparkConf = new SparkConf().setAppName("SparkStreamingApp").setMaster("local[2]");
-        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(30));
+        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(1));
 
         Set<String> topics = new HashSet<String>(Arrays.asList("testTopic".split(",")));
 
@@ -57,43 +60,54 @@ public class SparkStreamingService {
 
         //HBase Configuration
         Configuration conf = HBaseConfiguration.create();
+		
         // Instantiating HTable class
         Connection connection = ConnectionFactory.createConnection(conf);
         Table table = connection.getTable(TableName.valueOf("testhbase"));
 
-        // Instantiating Put class
-        Put p = new Put(Bytes.toBytes("row1"));
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 
         java.util.logging.Logger logger = java.util.logging.Logger.getLogger("logger1");
         logger.log(Level.INFO,"Logger Started");
 
-        System.out.println(messages.count());
         messages.foreachRDD(
                 (Function<JavaPairRDD<String, String>, Void>) rdd -> {
+					
                     logger.log(Level.INFO,"ForEach function..");
+					
                     System.out.println("Rdd Count"+rdd.count());
+					
                     List<Tuple2<String,String>> list = rdd.collect();
-                    for(Tuple2<String,String> tuple : list){
-                        logger.log(Level.INFO,"Reading Tuple");
-                        jsonMessage = tuple._2.toString();
-                        objectMapper = new ObjectMapper();
-                        iotData = objectMapper.readValue(jsonMessage, IOTModel.class);
+					
+                    for(Tuple2<String,String> tuple : list) {
+						
+                        logger.log(Level.INFO, "Reading Tuple");
+						
+                        jsonMessages = tuple._2.toString();
+						
+                        for (String jsonMessage : jsonMessages.split("\\$\\$")) {
+							
+							
+                            objectMapper = new ObjectMapper();
+                            iotData = objectMapper.readValue(jsonMessage, IOTModel.class);
 
-                        p.addColumn(Bytes.toBytes("data"),Bytes.toBytes("deviceId"),Bytes.toBytes(iotData.getData().getDeviceId()));
-                        p.addColumn(Bytes.toBytes("data"),Bytes.toBytes("temperature"),Bytes.toBytes(iotData.getData().getTemperature()));
-                        p.addColumn(Bytes.toBytes("data"),Bytes.toBytes("locLatitude"),Bytes.toBytes(iotData.getData().getLocation().getLatitude()));
-                        p.addColumn(Bytes.toBytes("data"),Bytes.toBytes("locLongitude"),Bytes.toBytes(iotData.getData().getLocation().getLongitude()));
+                            timestamp = new Date(Long.parseLong(iotData.getData().getTime()));
+							
+							// Instantiating Put class
+                            p = new Put(Bytes.toBytes(iotData.getData().getTime()));
 
-                        timestamp = new Date(Long.getLong(iotData.getData().getTime()));
+                            p.addColumn(Bytes.toBytes("data"), Bytes.toBytes("deviceId"), Bytes.toBytes(iotData.getData().getDeviceId()));
+                            p.addColumn(Bytes.toBytes("data"), Bytes.toBytes("temperature"), Bytes.toBytes(iotData.getData().getTemperature()));
+                            p.addColumn(Bytes.toBytes("data"), Bytes.toBytes("locLatitude"), Bytes.toBytes(iotData.getData().getLocation().getLatitude()));
+                            p.addColumn(Bytes.toBytes("data"), Bytes.toBytes("locLongitude"), Bytes.toBytes(iotData.getData().getLocation().getLongitude()));
+                            p.addColumn(Bytes.toBytes("data"), Bytes.toBytes("time"), Bytes.toBytes(sdf.format(timestamp)));
 
-                        p.addColumn(Bytes.toBytes("data"),Bytes.toBytes("time"),Bytes.toBytes(sdf.format(timestamp)));
-
-                        hTable.put(p);
+                            table.put(p);
 
                         System.out.println("data inserted");
                         logger.log(Level.INFO,"Ending Tuple");
+                        }
                     }
                     return null;
                 }
